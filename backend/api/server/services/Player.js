2fa747040db76b81c786a683677b44376e60f7f2
Jigar Chandra <chandra.67@osu.edu>
2021-02-22 20:20:48 +0530

    Add special data handling

--- backend/api/server/services/Player.js
+++ backend/api/server/services/Player.js
@@ -1,6 +1,7 @@
 const sequelize = require('sequelize');
 const db = require('../src/models');
 
+const FIELDS = Object.keys(db.Player.rawAttributes);
 class Player {
   static get(playerNameFilter, limit, offset, sortParams) {
     const query = {
@@ -26,12 +27,28 @@ class Player {
     if (sortParams) {
       const order = sortParams.reduce((acc, curr) => {
         const currOrder = curr.startsWith('-') ? 'DESC' : 'ASC';
-        const cleaned = curr.startsWith('-') ? curr.substring(1) : curr;
-        acc.push([cleaned, currOrder]);
+        let cleaned = curr.startsWith('-') ? curr.substring(1) : curr;
+        // sanitize Lng column
+        if (cleaned === 'Lng') {
+          query.attributes = [
+            [
+              sequelize.literal('CAST (REPLACE("Player".\'Lng\', \'T\', \'\') as INTEGER)'),
+              'cleaned_Lng'
+            ]
+          ];
+          cleaned = sequelize.literal(`cleaned_Lng ${currOrder}`);
+          acc.push([cleaned]);
+        } else {
+          acc.push([cleaned, currOrder]);
+        }
         return acc;
       }, []);
 
       query.order = order;
+    }
+
+    if (query.attributes) {
+      query.attributes.push(...FIELDS);
     }
 
     return db.Player.findAndCountAll(query).then((result) => {
